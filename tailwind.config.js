87bcbe3743eb6a8f6eae0bada21501d97e0258d4
Oakar <linn.998.mlm@gmail.com>
2023-11-11 15:14:01 +0630

    this is add tailwind lesson

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["nav.html", "test.html", "news.html", "buttoms.html", "table.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
