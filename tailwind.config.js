d1bf9805c0b17564da9ecdce098a517c83be14c8
Oakar <linn.998.mlm@gmail.com>
2023-11-12 08:39:10 +0630

    update layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["nav.html", "test.html", "news.html", "buttoms.html", "table.html"],
+  content: [
+    "nav.html",
+    "test.html",
+    "layout.html",
+    "news.html",
+    "buttoms.html",
+    "table.html",
+  ],
   theme: {
     extend: {},
   },
